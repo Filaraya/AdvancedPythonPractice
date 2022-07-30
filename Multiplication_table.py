454c903cb457a551ab55209a6d185ac9d0b86c4c
filaraya <filijosi@gmail.com>
2022-07-29 23:28:11 -0700

    Create Multiplication_table.py
    fileadded

---
+++ Multiplication_table.py
@@ -0,0 +1,18 @@
+#Multiplication Table
+"""
+Display a multiplication table for 1 throught 10 times 10.
+"""
+MIN = 1
+MAX = 10
+# Display the top row of labels
+print("    ", end="")
+for i in range (MIN, MAX+1):
+    print("%4d" %i, end= " ")
+print()
+
+#Display the table
+for i in range(MIN,MAX+1):
+    print("%4d" % i, end="")
+    for j in range(MIN,MAX+1):
+        print("%4d" % (i*j), end= " ")
+    print()
