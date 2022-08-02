62d13a50a5cb4eaeecd6a4e067db77b57235e129
filaraya <filijosi@gmail.com>
2022-08-01 23:26:29 -0700

    Update class_exer.py

--- class_exer.py
+++ class_exer.py
@@ -1,7 +1,16 @@
 #class declaration
 class Jungle:
+    #constructor with default values
+    def __init__(self, name="Unknown"):
+        self.visitorName = name
+        
     def welcomeMessage(self):
         print("Welcome to the Jungle")
 
 j= Jungle() #create object of class Jungle
 print(j.welcomeMessage()) #welcome to the Jungle
+
+#create object of class Jungle
+f = Jungle("Meher")
+f.welcomeMessage() # Hello Meher, Welcome to the Jungle
+
