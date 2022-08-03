17a86ade29ef06b746e9f96397b4b0c220314643
filaraya <filijosi@gmail.com>
2022-08-02 23:59:33 -0700

    Update class_exer.py

--- class_exer.py
+++ class_exer.py
@@ -14,3 +14,13 @@ print(j.welcomeMessage()) #welcome to the Jungle
 f = Jungle("Meher")
 f.welcomeMessage() # Hello Meher, Welcome to the Jungle
 
+def main():
+     #create object of class Jungle
+    j = Jungle("Meher")
+    j.welcomeMessage() # Hello Meher, Welcome to the Jungle
+    # if no name is passed, the default value i.e. Unknown will be used
+    k = Jungle()
+    k.welcomeMessage() # Hello Unknown, Welcome to the Jungle
+# standard boilerplate to set 'main' as starting function
+if __name__=='__main__':
+    main()
