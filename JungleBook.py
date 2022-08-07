f5bd2eccb2df00b7f46938c053c123317206d969
filaraya <filijosi@gmail.com>
2022-08-06 23:02:22 -0700

    update

--- JungleBook.py
+++ JungleBook.py
@@ -6,3 +6,13 @@ class Jungle:
         self.visitorName = name
     def welcomeMessage(self):
         print("Hello %s, Welcome to the Jungle" % self.visitorName)
+
+
+class RateJungle(Jungle):
+    def __init__(self, name, feedback):
+    # feedback (1-10) : 1 is the best. self.feedback = feedback # Public Attribute
+        # inheriting the constructor of the class
+        super().__init__(name)
+    # using parent class attribute i.e. visitorName
+    def printRating(self):
+        print("Thanks %s for your feedback" % self.visitorName)
