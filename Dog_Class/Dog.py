ce4081b40bef41a615e4ed057327b6bc857d801d
filaraya <filijosi@gmail.com>
2022-08-14 00:14:51 -0700

    Update Dog.py

--- Dog_Class/Dog.py
+++ Dog_Class/Dog.py
@@ -1,8 +1,17 @@
 #Dog.py
 
 class Dog: #declaration of dog class
-    pass
+    """to model a dog"""
+    def __init__(self,name,age):
+        """intialize name and age attributes."""
+        self.name = name
+        self.age = age
+        
+    def infoDog(self):
+        print(f"Hello my dog is {self.name} and {self.age} years old")
+        
+    
 
 
-s= Dog() #declaration of object
-print(s)
+s= Dog("Simba",4) #declaration of object
+print(s.infoDog())
