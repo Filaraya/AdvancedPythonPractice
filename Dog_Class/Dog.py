632da153bec8333ad8ce534e6f32555bd4207146
filaraya <filijosi@gmail.com>
2022-08-14 00:23:11 -0700

    Update Dog.py

--- Dog_Class/Dog.py
+++ Dog_Class/Dog.py
@@ -6,6 +6,16 @@ class Dog: #declaration of dog class
         """intialize name and age attributes."""
         self.name = name
         self.age = age
+    
+    #what can a dog can do it play and sound
+        
+    def play(self):
+        """asking a dog to play"""
+        print(f"{self.name} is now playing")
+        
+    def sound(self):
+        """when a dog make a sound/bark"""
+        print(f"{self.name} is making sound")
         
     def infoDog(self):
         print(f"Hello my dog is {self.name} and {self.age} years old")
@@ -15,3 +25,5 @@ class Dog: #declaration of dog class
 
 s= Dog("Simba",4) #declaration of object
 print(s.infoDog())
+print(s.play())
+print(s.sound())
