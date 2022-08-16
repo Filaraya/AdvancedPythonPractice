e153ced0295233b6f8a384025ff2fb461efd5dc9
filaraya <filijosi@gmail.com>
2022-08-16 00:01:25 -0700

    Update Car.py

--- Car_class/Car.py
+++ Car_class/Car.py
@@ -6,11 +6,24 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
-        
+        self.odometer = 0 #setting a default value of an attribute
+        self.color = " "
     def get_description(self):
         """Return the information of car"""
         info= f"this car is {self.make}, {self.model}, on {self.year}"
         return info.title()
     
+    def read_odometer(self):
+        """print car's milage"""
+        print(f"This is car has {self.odometer} milage")
+        
+    def car_color(self):
+        """info about car's color"""
+        print (f"the car color is {self.color} color")
+        
+    
 myCar = Car("Acura", "TL", "2003")
 print (myCar.get_description())
+print(myCar.read_odometer())
+myCar.odometer = 124000 #updating the new milage
+myCar.read_odometer()
