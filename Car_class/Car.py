125e20f7652b585c82433eed93d3a06bcb00142c
filaraya <filijosi@gmail.com>
2022-08-16 23:57:29 -0700

    Update Car.py

--- Car_class/Car.py
+++ Car_class/Car.py
@@ -7,7 +7,7 @@ class Car:
         self.model = model
         self.year = year
         self.odometer = 0 #setting a default value of an attribute
-        self.color = " "
+        #self.color = " "
     def get_description(self):
         """Return the information of car"""
         info= f"this car is {self.make}, {self.model}, on {self.year}"
@@ -16,14 +16,32 @@ class Car:
     def read_odometer(self):
         """print car's milage"""
         print(f"This is car has {self.odometer} milage")
+    
+    def update_odometer(self,mileage):
+        """update the car milage on the method"""
+        if mileage >= self.odometer:
+            self.odometer = mileage
+        else:
+            print ("You can't roll back an odometer")
+            
+    def incrimental_odometer(self,miles):
+        """adding the miles to the orginal reading"""
+        self.odometer += miles
         
+    """
     def car_color(self):
-        """info about car's color"""
+        #info about car's color
         print (f"the car color is {self.color} color")
-        
+        """
     
 myCar = Car("Acura", "TL", "2003")
 print (myCar.get_description())
 print(myCar.read_odometer())
 myCar.odometer = 124000 #updating the new milage
 myCar.read_odometer()
+myCar.update_odometer(126000)
+myCar.read_odometer()
+myCar.incrimental_odometer(10)
+myCar.read_odometer()
+
+
